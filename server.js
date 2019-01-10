b54688ee3eb029567035d9e1d728f3b824c46455
GBZUDD1 <45389289+GBZUDD1@users.noreply.github.com>
2019-01-10 17:12:36 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view-engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+     res.render('home.hbs');
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
