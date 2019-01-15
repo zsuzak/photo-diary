7b82e3505bf24e10ca187e012b92d4ac6f09debf
Zak <zakariyya.s.uddin@gmail.com>
2019-01-15 12:45:02 +0000

    Hopefully this is done

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const port = process.env.PORT || 3000;
 
 let app = express();
-let images;
+let images = "";
 let urls = [];
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -17,20 +17,24 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', async (req, res) => {
     await axios.get('https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=3f512a426170f3b1734a0f4d2dbf5048&user_id=jamessphotography&extras=url_l&per_page=999&format=json&nojsoncallback=1')
         .then((res) => {
-            for (let i = 0; i < res.data.photos.photo.length; i++) {
+            images += `<div class="left">`;
+            for (let i = 0; i < res.data.photos.photo.length; i+=2) {
                 let src = res.data.photos.photo[i].url_l;
-                if (i%2 == 0 ) {
-                    images += `<img class="left" src="${src}">`;
-                }
-                else {
-                    images += `<img class="right" src="${src}">`;
-                }
+                images += `<img src="${src}">`;
             }
+            images += `</div>`;
+            images += `<div class="right">`;
+            for (let i = 1; i < res.data.photos.photo.length; i+=2) {
+                let src = res.data.photos.photo[i].url_l;
+                images += `<img src="${src}">`;
+            }
+            images += `</div>`;
         })
         .catch((e) => {
             console.log(e);
         })
     res.render('home.hbs', {images});
+    images = "";
 });
 
 app.get('/about', (req, res) => {
