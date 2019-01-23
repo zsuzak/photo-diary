e93f36aac7f3cc4bd3835a5a67f9a21c15cdb167
Zak <zakariyya.s.uddin@gmail.com>
2019-01-23 16:12:19 +0000

    Experimenting with css

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', async (req, res) => {
     await axios.get('https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=3f512a426170f3b1734a0f4d2dbf5048&user_id=161576192@N05&extras=url_l%2C+date_taken&per_page=999&format=json&nojsoncallback=1')
         .then((res) => {
-            for (let i = 0; i < res.data.photos.photo.length; i++) {
-                let src = res.data.photos.photo[i].url_l;
-                let date_taken = res.data.photos.photo[i].datetaken;
+            let pics = res.data.photos.photo.sort((a,b) => {
+                return (a.datetaken > b.datetaken) ? -1 : ((a.datetaken < b.datetaken) ? 1 : 0);
+            });
+            
+            for (let i = 0; i < pics.length; i++) {
+                let src = pics[i].url_l;
+                let date_taken = pics[i].datetaken;
                 images += `<img src="${src}">`;
+                images += `<p>${date_taken}</p>`;
             }
         })
         .catch((e) => {
