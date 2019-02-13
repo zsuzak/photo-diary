b733bc234fd8d8aadf1a5d63bfd02634229bd0ee
Zak <zakariyya.s.uddin@gmail.com>
2019-02-13 13:52:27 +0000

    Added templating and small styling changes

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const axios = require('axios');
 const port = process.env.PORT || 3000;
 
 let app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view-engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 let images = "";
 let urls = [];
-
-hbs.registerPartials(__dirname + '/views/partials');
-
-app.set('view-engine', 'hbs');
-
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', async (req, res) => {
     await axios.get('https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=3f512a426170f3b1734a0f4d2dbf5048&user_id=161576192@N05&extras=url_l%2C+date_taken&per_page=999&format=json&nojsoncallback=1')
